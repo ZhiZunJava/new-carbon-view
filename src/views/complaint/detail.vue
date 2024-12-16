8a14446a07c5f801b846f8016d8af5835b11d89b
程安宁 <3308869544@qq.com>
2024-12-16 11:23:00 +0800

    feat: 违规投诉

--- src/views/complaint/detail.vue
+++ src/views/complaint/detail.vue
@@ -1,28 +1,153 @@
 <template>
+  <div class="main">
+    <el-breadcrumb separator="/" class="breadcrumb" style="padding: 0 20px">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item :to="{ path: '/complaint/list' }">投诉列表页</el-breadcrumb-item>
+      <el-breadcrumb-item>投诉详情页</el-breadcrumb-item>
+    </el-breadcrumb>
+    <div class="content">
+      <el-card v-loading="loading" shadow="hover" :body-style="{ padding: '12px', 'border-radius': '6px' }">
+        <div class="info-title">{{ title }}</div>
+        <div class="info-footer">
+          <span>发布时间：{{ parseTime(publishTime, '{y}-{m}-{d}') }}</span>
+          <span :style="statusColor">{{ status }}</span>
+        </div>
+        <div class="info-content" v-html="content"></div>
 
+        <div class="file-wrap" v-if="files">
+          <div class="file-title">附件</div>
+          <div class="file-list">
+            <div class="file-item" v-for="(file, index) in files">
+              <span class="file-item-title">附件{{ index + 1 }}. </span>
+              <a :href="file.fileUrl" @click="download(file.fileName, file.filePath)" target="_blank">{{ file.fileName
+                }}</a>
+            </div>
+          </div>
+        </div>
+      </el-card>
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
+import { getComplaint } from '../../api/bid/complaint/complaint';
+
+
+
 export default {
   name: "detail",
   methods: {
-    
+    download(fileName, filePath) {
+      axios({
+        method: 'get',
+        url: filePath, // 文件的 URL
+        responseType: 'blob', // 指定响应类型为 blob
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' // 请求头，视情况调整
+        }
+      })
+        .then(function (response) {
+          const blob = response.data;
+          const a = document.createElement('a');
+          const blobUrl = window.URL.createObjectURL(blob);
+
+          a.href = blobUrl;
+          a.setAttribute('download', fileName);
+          a.click();
+          window.URL.revokeObjectURL(blobUrl);
+        })
+        .catch(function (error) {
+          console.error('下载失败', error);
+        });
+    }
   },
   data() {
     return {
-      
+      loading: false,
+      type: '',
+      id: '',
+      data: {},
+      title: '',
+      content: '',
+      author: '',
+      publishTime: '',
+      files: [],
+      statusOptions: [],
+      status: '',
+      statusColor: {}
     }
   },
   async created() {
-    
+    this.id = this.$route.query.id
+    this.getDicts("complaint_status").then(response => {
+      this.statusOptions = response.data;
+    });
   },
   mounted() {
-    
+    // if (this.type === 'bidding') {
+    this.loading = true;
+    getComplaint(this.id).then(response => {
+      this.$nextTick(() => {
+        this.data = response.data
+        this.title = response.data.title
+        this.content = response.data.content
+        this.publishTime = response.data.createTime
+        this.files = response.data.filesList
+        this.status = this.selectDictLabel(this.statusOptions, response.data.status);
+        if(response.data.status === '0'){
+          this.statusColor['color'] = '#ffba00'
+        }
+        this.loading = false
+      })
+    })
+    // }
   },
 
 }
 </script>
 
 <style lang="scss" scoped>
+.content {
+  margin-top: 15px;
+  background: #FFFFFF;
+}
 
+.el-card {
+  border-radius: 10px !important;
+}
+
+.info-title {
+  margin-top: 25px;
+  text-align: center;
+  font-size: 30px;
+  font-weight: bold;
+  margin-bottom: 20px;
+  color: #333;
+  line-height: 40px;
+}
+
+.info-footer {
+  margin-top: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 50px;
+}
+
+.info-content {
+  position: relative;
+  font-size: 16px;
+  word-wrap: break-word;
+  line-height: 30px;
+  text-align: left;
+  margin: 50px 44px 0 36px;
+
+  p {
+    word-wrap: break-word;
+    word-break: normal;
+    line-height: 1.5;
+    margin-bottom: 1.7em;
+  }
+}
 </style>
--- src/views/complaint/list.vue
+++ src/views/complaint/list.vue
@@ -1,28 +1,218 @@
 <template>
-  
+  <div class="main">
+    <div style="display: flex; justify-content: space-between;align-items: center;">
+      <el-breadcrumb separator="/" class="breadcrumb" style="padding: 0 20px">
+        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item>投诉列表页</el-breadcrumb-item>
+      </el-breadcrumb>
+      <el-button class="goButton" @click="handleAdd()">去投诉</el-button>
+    </div>
+
+    <div>
+      <el-container>
+        <el-main v-loading="loading">
+          <div class="content">
+            <el-table v-loading="loading" :data="complaintList" @row-click="handleTableRow">
+              <el-table-column label="序号" type="index" width="55" align="center" />
+              <el-table-column label="投诉名称" align="center" prop="title" />
+              <el-table-column label="创建时间" align="center" prop="createTime" width="180">
+                <template slot-scope="scope">
+                  <span>{{ parseTime(scope.row.createTime, '{y}-{m}-{d}') }}</span>
+                </template>
+              </el-table-column>
+              <el-table-column label="处理状态" align="center" prop="status">
+                <div slot-scope="scope">
+                  <span :style="statusColor(scope.row)">{{ statusFormat(scope.row) }}</span>
+                </div>
+              </el-table-column>
+              <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+                <template slot-scope="scope">
+                  <el-button size="mini" type="text" icon="el-icon-edit"
+                    v-hasPermi="['bid:complaint:edit']">查看</el-button>
+                </template>
+              </el-table-column>
+            </el-table>
+
+            <pagination v-show="total > 0" :total="total" :page.sync="queryParams.pageNum"
+              :limit.sync="queryParams.pageSize" @pagination="getList" />
+          </div>
+        </el-main>
+      </el-container>
+    </div>
+
+
+    <!-- 添加或修改违规投诉对话框 -->
+    <el-dialog :title="title" :visible.sync="open" width="800px" append-to-body>
+      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+        <el-form-item label="投诉标题" prop="title">
+          <el-input v-model="form.title" placeholder="请输入投诉标题" />
+        </el-form-item>
+        <el-form-item label="投诉信息" prop="content">
+          <editor v-model="form.content" :min-height="192" />
+        </el-form-item>
+        <el-form-item label="联系人" prop="contact">
+          <el-input v-model="form.contact" placeholder="请输入联系人" />
+        </el-form-item>
+        <el-form-item label="联系电话" prop="contactTel">
+          <el-input v-model="form.contactTel" placeholder="请输入联系电话" />
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="submitForm">确 定</el-button>
+        <el-button @click="cancel">取 消</el-button>
+      </div>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+import { listComplaint, getComplaint, delComplaint, addComplaint, updateComplaint, exportComplaint } from "../../api/bid/complaint/complaint";
+import Editor from '@/components/MyEditor';
+
 export default {
   name: "list",
+  components: {
+    Editor,
+  },
   data() {
     return {
-     
-    }
+      // 遮罩层
+      loading: true,
+      // 总条数
+      total: 0,
+      // 违规投诉表格数据
+      complaintList: [],
+      // 弹出层标题
+      title: "",
+      // 是否显示弹出层
+      open: false,
+      // 处理状态字典
+      statusOptions: [],
+      // 查询参数
+      queryParams: {
+        pageNum: 1,
+        pageSize: 10,
+      },
+      // 表单参数
+      form: {},
+      // 表单校验
+      rules: {
+        title: [
+          { required: true, message: "投诉标题不能为空", trigger: "blur" }
+        ],
+        content: [
+          { required: true, message: "投诉信息不能为空", trigger: "blur" }
+        ],
+        contact: [
+          { required: true, message: "联系人不能为空", trigger: "blur" }
+        ],
+        contactTel: [
+          { required: true, message: "联系电话不能为空", trigger: "blur" }
+        ],
+      },
+    };
   },
   created() {
-  
+    this.getList();
+    this.getDicts("complaint_status").then(response => {
+      this.statusOptions = response.data;
+    });
   },
   methods: {
-   
+    handleTableRow(row, column, event) {
+      this.$router.push(`/complaint/detail?id=${row.id}`)
+    },
+    /** 查询违规投诉列表 */
+    getList() {
+      this.loading = true;
+      listComplaint(this.queryParams).then(response => {
+        this.complaintList = response.rows;
+        this.total = response.total;
+        this.loading = false;
+      });
+    },
+    // 处理状态字典翻译
+    statusFormat(row, column) {
+      return this.selectDictLabel(this.statusOptions, row.status);
+    },
+    statusColor(row, column) {
+      if (row.status === '0') {
+        return {
+          color: '#ffba00'
+        }
+      }
 
+      return {}
+    },
+    // 取消按钮
+    cancel() {
+      this.open = false;
+      this.reset();
+    },
+    // 表单重置
+    reset() {
+      this.form = {
+        id: null,
+        enterpriseId: null,
+        title: null,
+        content: null,
+        contact: null,
+        contactTel: null,
+        status: '0',
+        createTime: null,
+        updateBy: null,
+        updateTime: null,
+        remark: null,
+      };
+      this.resetForm("form");
+    },
+    /** 新增按钮操作 */
+    handleAdd() {
+      this.reset();
+      this.open = true;
+      this.title = "投诉";
+    },
+    /** 提交按钮 */
+    submitForm() {
+      this.$refs["form"].validate(valid => {
+        if (valid) {
+          this.form.enterpriseId = this.userId;
+
+          addComplaint(this.form).then(response => {
+            this.msgSuccess("新增成功");
+            this.open = false;
+            this.getList();
+          });
+        }
+      });
+    },
   },
-  computed:{
-   
+  computed: {
+    ...mapGetters(['userId'])
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
+.content {
+  background: #FFFFFF;
+  border-radius: 10px;
+  opacity: 1;
+  min-height: 860px;
+  padding: 24px 21px 0 21px;
+}
 
+.goButton {
+  width: 120px;
+  height: 36px;
+  border-radius: 4px;
+  opacity: 1;
+  background: linear-gradient(90deg, #22CDB5 8%, #46DFCA 96%);
+  font-family: MicrosoftYaHei, sans-serif;
+  font-size: 16px;
+  font-weight: bold;
+  letter-spacing: 0em;
+  color: #FFFFFF;
+}
 </style>
